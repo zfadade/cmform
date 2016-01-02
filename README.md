8092ce80f2afe472c90bc9d81ffd290385e89d04
zfadade <zfadade@yahoo.com>
2016-01-02 12:21:09 -0500

    Make sublime blog project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Simple Blog part 1: The Build
 =============
 
-These files acompany the tutorial: [Creating a blog from scratch with PHP](http://daveismyname.com/creating-a-blog-from-scratch-with-php-bp)
+These files accompany the tutorial: [Creating a blog from scratch with PHP](http://daveismyname.com/creating-a-blog-from-scratch-with-php-bp)
--- index.php
+++ index.php
@@ -21,10 +21,10 @@
 				while($row = $stmt->fetch()){
 					
 					echo '<div>';
-						echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-						echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-						echo '<p>'.$row['postDesc'].'</p>';				
-						echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
+					echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+					echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
+					echo '<p>'.$row['postDesc'].'</p>';				
+					echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';				
 					echo '</div>';
 
 				}
@@ -35,7 +35,6 @@
 		?>
 
 	</div>
-
 
 </body>
 </html>
