6783785400df88538df3121b346e017fbb7d3557
zfadade <zfadade@yahoo.com>
2016-01-14 16:08:27 -0500

    Get DB working on 1&1; don't hardcode timezone

--- includes/config.php
+++ includes/config.php
@@ -2,22 +2,21 @@
 ob_start();
 session_start();
 
-
 $my_init_data = parse_ini_file("data.ini");
 $db_host = $my_init_data['cmblog_db_host'];
 $db_name = $my_init_data['cmblog_db_name'];
 $db_username = $my_init_data['cmblog_db_username'];
 $db_password = $my_init_data['cmblog_db_password'];
 
-$db = new PDO("mysql:host=".$db_host.";port=8889;dbname=".$db_name, 
+// port=8889;  the default configuration for MAMP uses ports 8888 and 8889, and 7888
+$db = new PDO("mysql:host=".$db_host.";dbname=".$db_name, 
       $db_username, 
       $db_password);
-//$db = new PDO("mysql:host=".DBHOST.";port=8889;dbname=".DBNAME, DBUSER, DBPASS);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 //set timezone
-date_default_timezone_set('America/New_York');
+date_default_timezone_set($my_init_data['default_timezone']);
 
 //load classes as needed
 function __autoload($class) {
